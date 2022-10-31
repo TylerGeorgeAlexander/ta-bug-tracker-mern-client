2712138fb2d20959b4dbf22ca593ed3142512598
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-31 14:06:19 -0700

    style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,9 +7,11 @@ const Loader = () => {
       <div className="loader">
         <Spinner size={50} />
       </div>
-      <span className="text-center">
-        Current Heroku hosting may take a few minutes to spin up.
-      </span>
+      <div className="flex">
+        <span className="text-center">
+          Current Heroku hosting may take a few minutes to spin up.
+        </span>
+      </div>
     </>
   );
 };
