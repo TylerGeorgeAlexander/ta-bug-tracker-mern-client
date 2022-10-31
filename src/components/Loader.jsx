0be1b202c0316b230015db8e7d2b14348c4cfb4c
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-31 12:44:11 -0700

    style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,10 +3,14 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <Spinner size={50} />
-      <span className="text-center">Current Heroku hosting may take a few minutes to spin up.</span>
-    </div>
+    <>
+      <div className="loader">
+        <Spinner size={50} />
+      </div>
+      <span className="text-center">
+        Current Heroku hosting may take a few minutes to spin up.
+      </span>
+    </>
   );
 };
 
